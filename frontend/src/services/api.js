2e8fe32b9b7ed0015a6762b214ab6ba6259ba245
Nareshroyal0212 <nareshkumarreddy0212@gmail.com>
2026-08-24 18:09:54 +0530

    Use production API environment variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,35 +1,30 @@
 import axios from "axios";
 
 const API = axios.create({
-//baseURL: "http://localhost:9090/api",
-    baseURL: "https://restauranthub-backend-bo2t.onrender.com/api",
+    // Local development:
+    // baseURL: "http://localhost:9090/api",
+
+    // Production - Vercel uses VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL,
 
     headers: {
-
         "Content-Type": "application/json"
-
     }
-
 });
 
 API.interceptors.request.use(
-
     (config) => {
-
         const token = localStorage.getItem("token");
 
         if (token) {
-
             config.headers.Authorization = `Bearer ${token}`;
-
         }
 
         return config;
-
     },
-
-    (error) => Promise.reject(error)
-
+    (error) => {
+        return Promise.reject(error);
+    }
 );
 
 export default API;
